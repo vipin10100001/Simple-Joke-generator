cead23d0d054372d94feaa6832236f0a034410b5
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2023-09-03 18:30:48 +0530

    first commit
    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const jokeContainer = document.getElementById('joke');
+const btn=document.getElementById('btn');
+const url='https://api.chucknorris.io/jokes/random';
+
+let getJoke=()=>{
+    jokeContainer.classList.remove('fade');
+    fetch(url)
+    .then(data=>data.json())
+    .then(item=>{
+        jokeContainer.innerHTML=item.value;
+        jokeContainer.classList.add('fade');
+
+    })};
+
+btn.addEventListener('click',getJoke);
